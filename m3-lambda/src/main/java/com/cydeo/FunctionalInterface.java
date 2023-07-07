bbc3d1c037cf1475dcd7ec5f56d5231304f64a9a
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-07-07 14:42:48 +0300

    we learn anonymous class and functional interfaces.

---
+++ m3-lambda/src/main/java/com/cydeo/FunctionalInterface.java
@@ -0,0 +1,83 @@
+package com.cydeo;
+
+import com.cydeo.apple.Apple;
+import com.cydeo.apple.ApplePredicate;
+
+import java.util.function.*;
+
+public class FunctionalInterface {
+    public static void main(String[] args) {
+
+        //Below we learn anonymous class. When we create an object from an interface with new keyword it automatically creates an anonymous class like this and implement the necessary abstract method.
+        ApplePredicate ap = new ApplePredicate() {
+            @Override
+            public boolean test(Apple apple) {
+                return apple.getWeight()>200;
+            }
+        };
+
+        System.out.println("************PREDICATE***************");
+        Predicate<Integer> lesserThan =  i -> i<18;
+        System.out.println(lesserThan.test(20));
+
+        System.out.println("************CONSUMER***************");
+        Consumer<Integer> display = i -> System.out.println(i);
+        display.accept(10);
+
+        System.out.println("************BI CONSUMER***************");
+        BiConsumer<Integer,Integer> addTwo = (x,y) -> System.out.println(x+y);
+        addTwo.accept(1,2);
+
+        System.out.println("************FUNCTION***************");
+        Function<String,String> fun = s -> "Hello " + s;
+        System.out.println(fun.apply("Cydeo"));
+
+        System.out.println("************BI FUNCTION***************");
+        BiFunction<Integer,Integer,Integer> func = (x1,x2) -> x1+x2;
+        System.out.println(func.apply(2,3));
+
+
+        System.out.println("************SUPPLIER***************");
+        Supplier<Double> randomValue = () -> Math.random();
+        System.out.println(randomValue.get());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

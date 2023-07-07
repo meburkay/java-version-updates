7c509b72e083868e42a26793766f9062062384c8
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-07-07 11:45:59 +0300

    We implement lambda to old way.

--- m3-lambda/src/main/java/com/cydeo/sorting/ArraySorting.java
+++ m3-lambda/src/main/java/com/cydeo/sorting/ArraySorting.java
@@ -6,16 +6,23 @@ public class ArraySorting { //QuickSort,BubbleSort
 
         ArraySorting as = new ArraySorting();
 
+        //We create object from QuickSort class.
         QuickSort qs = new QuickSort();
         BubbleSort bs = new BubbleSort();
 
+        //We use the created objects to use the actions of them.
         as.sort(qs);
         as.sort(bs);
 
+        //---------------------------------------------------------
+
+        //With lambda we bypass the classes and only use the interface for passing the action to method.
         Sorting quickSort = () -> System.out.println("Quick Sorting");
         as.sort(quickSort);
 
         as.sort(() -> System.out.println("Quick Sorting"));
+
+        //Java 8 has a lot of ready interfaces for different situations. by using them we can bypass the interface we create too. By that without creating interface, classes, objects we pass the action we want to the method.
 
 
 

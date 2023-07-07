4497490e446c183048a14ec5a2ce4234c195c357
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-07-07 13:54:37 +0300

    Another lambda example day56 lambda tasks question 1.

---
+++ m3-lambda/src/main/java/com/cydeo/tasks/orange/Orange.java
@@ -0,0 +1,16 @@
+package com.cydeo.tasks.orange;
+
+import com.cydeo.apple.Color;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Orange {
+    private int weight;
+    private Color color;
+}
---
+++ m3-lambda/src/main/java/com/cydeo/tasks/orange/OrangeFormatter.java
@@ -0,0 +1,10 @@
+package com.cydeo.tasks.orange;
+
+@FunctionalInterface
+public interface OrangeFormatter {
+    String accept(Orange orange);
+}
+
+/*
+Day56 lambda tasks question 1
+ */

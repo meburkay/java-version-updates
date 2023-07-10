586c28174f160157f65bddf37e787a2500df4d74
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-07-10 11:41:21 +0300

    2 generic methods created.

--- m1-generics/src/main/java/com/cydeo/review/GenericsTest.java
+++ m1-generics/src/main/java/com/cydeo/review/GenericsTest.java
@@ -18,10 +18,34 @@ public class GenericsTest {
         teacherList.add(new Teacher("Albus", 201));
         teacherList.add(new Teacher("Severus", 202));
 
+//        printInfo(studentList);
+//        printInfo(teacherList);
+
+//        printInfo2(studentList);
+//        printInfo2(teacherList);
+
+        System.out.println(getLastItem(studentList));
+        System.out.println(getLastItem(teacherList));
+
+    }
+
+    // Write a method that takes a list of students, prints each student and the total student count
+    public static void printInfo(List<Student> students) {
+        for (Student student : students) {
+            System.out.println(student);
+        }
+        System.out.println(students.size());
+    }
+
+    public static <T> void printInfo2(List<T> personList) {
+        for (T person : personList) {
+            System.out.println(person);
+        }
+        System.out.println(personList.size());
+    }
+
+    public static <T> T getLastItem(List<T> list) {
+        return list.get(list.size() - 1);
     }
 
 }
-
-    // Write a method that takes a list of students, prints each student and the total student count
-
-

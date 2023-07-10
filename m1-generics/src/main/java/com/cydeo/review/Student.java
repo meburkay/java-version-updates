39d50907f3d987209ed2e7fbdd28ca1e3134a6a0
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-07-10 22:52:02 +0300

    for the 3 modules lombok dependency added and lombok used at Student class.

--- m1-generics/src/main/java/com/cydeo/review/Student.java
+++ m1-generics/src/main/java/com/cydeo/review/Student.java
@@ -1,37 +1,18 @@
 package com.cydeo.review;
 
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Data
 public class Student extends Person {
 
     private String name;
     private int id;
-
-    public Student(String name, int id) {
-        this.name = name;
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                '}';
-    }
 
 }

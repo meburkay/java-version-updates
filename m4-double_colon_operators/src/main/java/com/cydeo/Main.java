313d7f3fe968b83d71b60266e6f7177e08db0baa
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-07-12 20:06:38 +0300

    m4-double_colon_operators

--- m4-double_colon_operators/src/main/java/com/cydeo/Main.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -1,10 +1,37 @@
 package com.cydeo;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Calculate sum = (x,y) -> System.out.println(x+y);
 
+        //If we have a method that have the same signature as the functional interface's abstract method. We can use that method as the body/implementation of the lambda expression.
+        Calculate s1 = (x,y) -> Calculator.findSum(x,y);
+
+        //If we reference another method to our lambda expression's implementation. We can use double colon operator and when we are using double colon operator we do not need to write parameters. Because the compiler already understands it from the functional interface or the method that referenced.
+
+        //Reference to a Static Method
+        Calculate s2 = Calculator::findSum;
+        s2.calculate(10,20);
+
+        //Reference to a instance method
+        Calculator obj = new Calculator();
+        Calculate s3 = obj ::findMultiply;
+
+        Calculate s4 = new Calculator() ::findMultiply;
+
+
+        BiFunction<String,Integer,String> func = (str, i) -> str.substring(i);
+        //Here we can call String class normal method directly because of the String parameter that the func2 have. I explain it below in detail.
+        BiFunction<String,Integer,String> func2 = String::substring;
+
+        Function<Integer,Double> b = new MyClass()::method;
+        //If our functional interface have the class as a parameter, we do not need to create an object from that class. We can call it like static which is showed below.
+        BiFunction<MyClass,Integer,Double> b1 = MyClass::method;
 
     }
 }

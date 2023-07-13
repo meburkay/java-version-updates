3f3dad7db425dce7f1e175f79b96861f15026172
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-07-13 10:34:49 +0300

    m5-comparator interface usage of old version and lambda

---
+++ m5-comparator_interface/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,27 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(10,0,15,5,20);
+        System.out.println(list);
+
+
+        //Ascending
+        Collections.sort(list);
+        System.out.println(list);
+
+        //Here we use the overload of sort method and it accepts Comparator interface as the second parameter. At first we use the old way and create our interface which implements Comparator interface and give it as argument. And as the second example we use lambda directly and do the implementation right here. Thus we did not use our interface or any other thing.
+        //Descending
+        Collections.sort(list,new MyComparator());
+        System.out.println(list);
+
+        Collections.sort(list,((o1,o2) -> (o1>o2) ? -1 : (o1<o2) ? 1 : 0));
+
+    }
+}

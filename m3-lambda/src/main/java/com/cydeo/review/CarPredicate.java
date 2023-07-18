0f73af9ee57e8b709d5c4179736d3c8224256992
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-07-18 15:21:58 +0300

    Review - FunctionalInterface annotation purpose

--- m3-lambda/src/main/java/com/cydeo/review/CarPredicate.java
+++ m3-lambda/src/main/java/com/cydeo/review/CarPredicate.java
@@ -1,5 +1,6 @@
 package com.cydeo.review;
 
+@FunctionalInterface//We are using it to warn our self or the other programmers that this interface is functional. By putting this here if anybody forget and try to implement any other abstract method here compiler automatically worn us or the other programmers.
 public interface CarPredicate {
 
     boolean test(Car car);

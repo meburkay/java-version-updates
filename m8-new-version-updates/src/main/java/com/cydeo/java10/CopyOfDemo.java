e2a4bd4394c1896fa0dd802a48d84469e8dfc416
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-07-21 12:32:28 +0300

    java 10 updates copyOf

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java10/CopyOfDemo.java
@@ -0,0 +1,19 @@
+package com.cydeo.java10;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CopyOfDemo {
+
+    public static void main(String[] args) {
+
+
+        List<Integer> numberList = Arrays.asList(2,4,6,8);
+
+        //with copyOf we give collection directly to create an unmodifiable list.
+        List<Integer> unmodifiableList2= List.of(2,4,6,8);
+        List<Integer> unmodifiableList = List.copyOf(numberList);
+
+    }
+
+}
